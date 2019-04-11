cc8ffbb5b6a1601db9996fc7f620b66adaa6aac9
Dirac <1063238990@qq.com>
2019-04-11 19:41:17 +0800

    [ADD]some content

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+dokcerfile_path="src/main/docker"
+image_name="webapp:latest"
+mvn clean package -Dmaven.test.skip=true \
+&& cp target/webapp.jar ${dokcerfile_path} \
+&& cd ${dokcerfile_path} \
+&& docker build -t ${image_name} . \
